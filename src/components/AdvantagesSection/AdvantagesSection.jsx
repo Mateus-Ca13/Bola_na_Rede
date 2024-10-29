79b39a6f3473f86edc918e533ed5b97cfa9c0ffa
Mateus Cavichion <mateuscavichion9@gmail.com>
2024-10-29 00:22:38 -0300

    Adicionando componentes de interface na landing page.

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -4,8 +4,8 @@ import { IoShieldCheckmarkOutline } from "react-icons/io5";
 export default function AdvantagesSection() {
     const advantageData = [
         { id: 1, icon: <IoShieldCheckmarkOutline className='text-5xl' />, text: "Facilidade na organização" },
-        { id: 1, icon: <IoShieldCheckmarkOutline className='text-5xl' />, text: "Utilize de onde quiser" },
-        { id: 1, icon: <IoShieldCheckmarkOutline className='text-5xl' />, text: "Jogue com segurança" },
+        { id: 2, icon: <IoShieldCheckmarkOutline className='text-5xl' />, text: "Utilize de onde quiser" },
+        { id: 3, icon: <IoShieldCheckmarkOutline className='text-5xl' />, text: "Jogue com segurança" },
     ]
     return (
         <section className='mb-6  text-center'>
@@ -15,7 +15,7 @@ export default function AdvantagesSection() {
             </div>
             <div className='flexbox gap-2 my-4'>
                 {advantageData.map((adv) => {
-                    return <div className='font-medium text-xs h-36 w-24  bg-gradient-to-r from-blue3 to-blue4 text-white columnbox gap-3 rounded-md shadow-md'>
+                    return <div key={adv.id} className='font-medium text-xs h-36 w-24  bg-gradient-to-r from-blue3 to-blue4 text-white columnbox gap-3 rounded-md shadow-md'>
                         {adv.icon}
                         <p className='px-2'>{adv.text}</p>
                     </div>
--- src/components/Form/FormInput/FormInput.jsx
+++ src/components/Form/FormInput/FormInput.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function FormInput({label, type, placeholder}) {
   return (
     <div className='flex flex-col items-start my-2 '>
-        <label for={label}
+        <label htmlFor={label}
         className='font-semibold'>{label}</label>
         
         <input id={label} type={type} placeholder={placeholder}
